c3fc4949a307363281297b387089c89d47c6057c
mehul pawar <85021266+rocky-oos@users.noreply.github.com>
2023-10-21 06:14:27 +0000

    indesxlove

---
+++ index.js
@@ -0,0 +1,8 @@
+alert("Hey Hii , Plz Check Your Love < How Much Love Your BF/Gf to you : Thank you !")
+prompt("Enter Your BF Name");
+prompt("Enter Your GF Name");
+var Love = Math.random();
+var checkLove=Math.floor();
+alert("His/shes Love You is :" +checkLove+ " %");
+
+
